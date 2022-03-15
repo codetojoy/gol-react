42a5552e6d1777b7496b1616bacab39efd40546a
Michael Easter <codetojoy@gmail.com>
2022-03-15 15:16:57 -0300

    clean up for Config page

--- src/pages/Config/Config.tsx
+++ src/pages/Config/Config.tsx
@@ -1,55 +1,66 @@
-import React from "react";
+import React, { useRef, FormEvent } from "react";
 import { useAppSelector, useAppDispatch } from "../../hooks";
 import { setNumRows, setNumCols } from "../../store/config/actions";
 import { resetGrid } from "../../store/grid/actions";
 
-const Config: React.FC<{}> = (props) => {
-  const config = useAppSelector((state) => state.config);
-  const dispatch = useAppDispatch();
-  const rowClickHandler = () => {
-    console.log(`TRACER row click`);
-    const numRows = config.numRows + 1;
-    dispatch(setNumRows(numRows));
-    dispatch(resetGrid(numRows, config.numCols));
-  };
-  const colClickHandler = () => {
-    console.log(`TRACER col click`);
-    dispatch(setNumCols(config.numCols + 1));
-  };
-  return (
-    <div>
-      <p>TRACER Config</p>
-      <p>TRACER # rows: {config.numRows}</p>
-      <p>TRACER # cols: {config.numCols}</p>
-      <button onClick={rowClickHandler}>bump row value</button>
-      <button onClick={colClickHandler}>bump col value</button>
-    </div>
-  );
-};
-
-export default Config;
-
-/*
-// import { useEffect, useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-// import { NavLink, useHistory } from "react-router-dom";
-
-// const isLoggedIn = useSelector(state: RootState => state.user.loggedIn);
 import classes from "./Config.module.css";
 
-// https://github.com/pongsatt/redux-ts-demo/blob/master/src/store/index.ts
 const Config: React.FC<{}> = (props) => {
-  const config = useSelector<IRootState, boolean>((state: DefaultRootState) => {
-    return state.config;
-  });
+  const config = useAppSelector((state) => state.config);
+  const rowRef = useRef<HTMLInputElement>(null);
+  const colRef = useRef<HTMLInputElement>(null);
+  const dispatch = useAppDispatch();
+
+  const updateGrid = (numRows: number, numCols: number) => {
+    dispatch(resetGrid(numRows, numCols));
+  };
+  const rowClickHandler = () => {
+    const numRows: number = +rowRef!.current!.value;
+    const numCols: number = +colRef!.current!.value;
+    dispatch(setNumRows(numRows));
+    updateGrid(numRows, numCols);
+  };
+  const colClickHandler = () => {
+    const numRows: number = +rowRef!.current!.value;
+    const numCols: number = +colRef!.current!.value;
+    dispatch(setNumCols(numCols));
+    dispatch(resetGrid(numRows, numCols));
+  };
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+    const numRows: number = +rowRef!.current!.value;
+    const numCols: number = +colRef!.current!.value;
+    console.log(`TRACER submit ${numRows} ${numCols}`);
+  };
+
   return (
     <div className={classes.config}>
-      <h2>Config here</h2>
-      <p># rows: {config.numRows}</p>
-      <p># columns: {config.numCols}</p>
+      <h2>Config</h2>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="num-rows"># rows: </label>
+        <input
+          ref={rowRef}
+          onChange={rowClickHandler}
+          value={config.numRows}
+          id="num-rows"
+          type="number"
+          min="2"
+          max="20"
+        />
+        <br />
+        <label htmlFor="num-cols"># cols: </label>
+        <input
+          ref={colRef}
+          onChange={colClickHandler}
+          value={config.numCols}
+          id="num-cols"
+          type="number"
+          min="2"
+          max="20"
+        />
+      </form>
     </div>
   );
 };
 
 export default Config;
-*/
